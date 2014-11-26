051e7366c27f0662ee3a28e0cdfd651532e24427
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-11-26 15:23:49 -0500

    did some cleanup in loops and whatnot

--- Source_Files/GameWorld/effects.cpp
+++ Source_Files/GameWorld/effects.cpp
@@ -1,5 +1,5 @@
 /*
-EFFECTS.C
+EFFECTS.CPP
 
 	Copyright (C) 1991-2001 and beyond by Bungie Studios, Inc.
 	and the "Aleph One" developers.
@@ -48,14 +48,6 @@ Aug 30, 2000 (Loren Petrich):
 	ryan reports get_object_data() failing on effect->data after a teleport effect terminates
 */
 
-/* ---------- macros */
-
-/* ---------- structures */
-
-/* ---------- private prototypes */
-
-/* ---------- globals */
-
 /* import effect definition constants, structures and globals */
 #include "effect_definitions.h"
 
@@ -82,73 +74,76 @@ effect_definition *get_effect_definition(const short type)
 }
 
 
-short new_effect(
-	world_point3d *origin,
-	short polygon_index,
-	short type,
-	angle facing)
+short new_effect(world_point3d *origin, short polygon_index, short type, angle facing)
 {
-	short effect_index= NONE;
+	int16 effect_index = NONE;
 
-	if (polygon_index!=NONE)
-	{
-		struct effect_data *effect;
-		struct effect_definition *definition;
-	
-		definition= get_effect_definition(type);
-		// LP change: idiot-proofing
-		if (!definition) return NONE;
+	if (polygon_index == NONE)
+		return NONE;
 		
-		if (definition->flags&_sound_only)
-		{
-			struct shape_animation_data *animation= get_shape_animation_data(BUILD_DESCRIPTOR(definition->collection, definition->shape));
-			if (!animation) return NONE;
-			
-			play_world_sound(polygon_index, origin, animation->first_frame_sound);
-		}
-		else
-		{
-			for (effect_index= 0,effect= effects; effect_index<MAXIMUM_EFFECTS_PER_MAP; ++effect_index, ++effect)
-			{
-				if (SLOT_IS_FREE(effect))
-				{
-					short object_index= new_map_object3d(origin, polygon_index, BUILD_DESCRIPTOR(definition->collection, definition->shape), facing);
-					
-					if (object_index!=NONE)
-					{
-						struct object_data *object= get_object_data(object_index);
-						
-						effect->type= type;
-						effect->flags= 0;
-						effect->object_index= object_index;
-						effect->data= 0;
-						effect->delay= definition->delay ? global_random()%definition->delay : 0;
-						MARK_SLOT_AS_USED(effect);
-						
-						SET_OBJECT_OWNER(object, _object_is_effect);
-						object->sound_pitch= definition->sound_pitch;
-						if (effect->delay) SET_OBJECT_INVISIBILITY(object, true);
-						if (definition->flags&_media_effect) SET_OBJECT_IS_MEDIA_EFFECT(object);
-					}
-					else
-					{
-						effect_index= NONE;
-					}
-					
-					break;
-				}
-			}
-			if (effect_index==MAXIMUM_EFFECTS_PER_MAP) effect_index= NONE;
-		}
-	}
+	effect_data *effect;
+	effect_definition *definition = get_effect_definition(type);
 	
+	// LP change: idiot-proofing
+	if (!definition) 
+		return NONE;
+	
+	if (definition->flags & _sound_only)
+	{
+		shape_animation_data *animation = get_shape_animation_data(BUILD_DESCRIPTOR(definition->collection, definition->shape));
+		if (!animation) 
+			return NONE;
+		
+		play_world_sound(polygon_index, origin, animation->first_frame_sound);
+		return effect_index;
+	}
+
+	
+	for (effect_index= 0,effect= effects; effect_index<MAXIMUM_EFFECTS_PER_MAP; ++effect_index, ++effect)
+	{
+		if( !SLOT_IS_FREE(effect) )
+			continue;
+		
+		auto object_index = new_map_object3d(origin, polygon_index, 
+			BUILD_DESCRIPTOR(definition->collection, definition->shape), 
+			facing);
+		
+		if (object_index == NONE)
+		{
+			effect_index = NONE;
+			break;
+		}
+		
+		Object *object = get_object_data(object_index);
+		
+		effect->type		= type;
+		effect->flags		= 0;
+		effect->object_index	= object_index;
+		effect->data		= 0;
+		effect->delay		= definition->delay 
+					? global_random( definition->delay )
+					: 0;
+		MARK_SLOT_AS_USED(effect);
+		
+		SET_OBJECT_OWNER(object, _object_is_effect);
+		object->sound_pitch = definition->sound_pitch;
+		
+		if (effect->delay) 
+			SET_OBJECT_INVISIBILITY(object, true);
+		if (definition->flags & _media_effect) 
+			SET_OBJECT_IS_MEDIA_EFFECT(object);
+		break;
+	}
+	if(effect_index == MAXIMUM_EFFECTS_PER_MAP) 
+		effect_index = NONE;
+
 	return effect_index;
 }
 
 /* assumes ¶t==1 tick */
 void update_effects()
 {
-	struct effect_data *effect;
+	effect_data *effect;
 	short effect_index;
 	
 	for (effect_index= 0, effect= effects; effect_index<MAXIMUM_EFFECTS_PER_MAP; ++effect_index, ++effect)
@@ -193,87 +188,85 @@ void update_effects()
 	}
 }
 
-void remove_effect(
-	short effect_index)
+void remove_effect(int16 effect_index)
 {
-	struct effect_data *effect;
-	
-	effect= get_effect_data(effect_index);
+	effect_data *effect = get_effect_data(effect_index);
 	remove_map_object(effect->object_index);
+	
+	//lua hook
 	L_Invalidate_Effect(effect_index);
+	
 	MARK_SLOT_AS_FREE(effect);
 }
 
-void remove_all_nonpersistent_effects(
-	void)
+void remove_all_nonpersistent_effects()
 {
-	struct effect_data *effect;
+	effect_data *effect;
 	short effect_index;
 	
 	for (effect_index= 0, effect= effects; effect_index<MAXIMUM_EFFECTS_PER_MAP; ++effect_index, ++effect)
 	{
-		if (SLOT_IS_USED(effect))
-		{
-			struct effect_definition *definition= get_effect_definition(effect->type);
-			// LP change: idiot-proofing
-			if (!definition) continue;
-
-			if (definition->flags&(_end_when_animation_loops|_end_when_transfer_animation_loops))
-			{
-				remove_effect(effect_index);
-			}
-		}
-	}
-}
-
-void mark_effect_collections(
-	short effect_type,
-	bool loading)
-{
-	if (effect_type!=NONE)
-	{
-		struct effect_definition *definition= get_effect_definition(effect_type);
-		// LP change: idiot-proofing
-		if (!definition) return;
-
-		/* mark the effect collection */
-		loading ? mark_collection_for_loading(definition->collection) : mark_collection_for_unloading(definition->collection);
-	}
-}
-
-void teleport_object_out(
-	short object_index)
-{
-	struct object_data *object= get_object_data(object_index);
-	
-	if (!OBJECT_IS_INVISIBLE(object))
-	{
-		short effect_index= new_effect(&object->location, object->polygon, _effect_teleport_object_out, object->facing);
+		if (!SLOT_IS_USED(effect))
+			continue;
 		
-		if (effect_index!=NONE)
-		{
-			struct effect_data *effect= get_effect_data(effect_index);
-			struct object_data *effect_object= get_object_data(effect->object_index);
-			
-			// make the effect look like the object
-			effect_object->shape= object->shape;
-			effect_object->sequence= object->sequence;
-			effect_object->transfer_mode= _xfer_fold_out;
-			effect_object->transfer_period= TELEPORTING_MIDPOINT;
-			effect_object->transfer_phase= 0;
-			effect_object->flags|= object->flags&(_object_is_enlarged|_object_is_tiny);
-			
-			// make the object invisible
-			SET_OBJECT_INVISIBILITY(object, true);
+		effect_definition *definition = get_effect_definition(effect->type);
+		// LP change: idiot-proofing
+		if (!definition) 
+			continue;
 
-			play_object_sound(effect->object_index, Sound_TeleportOut()); /* teleport in sound, at destination */
-		}
+		if (definition->flags&(_end_when_animation_loops|_end_when_transfer_animation_loops))
+			remove_effect(effect_index);
 	}
+}
+
+void mark_effect_collections(short effect_type, bool loading)
+{
+	if (effect_type == NONE)
+		return;
+	
+	effect_definition *definition = get_effect_definition(effect_type);
+	// LP change: idiot-proofing
+	if (!definition) 
+		return;
+
+	/* mark the effect collection */
+	if(loading)
+		mark_collection_for_loading(definition->collection);
+	else
+		mark_collection_for_unloading(definition->collection);
+}
+
+void teleport_object_out(short object_index)
+{
+	Object *object = get_object_data(object_index);
+	
+	if (OBJECT_IS_INVISIBLE(object))
+		return;
+
+	auto effect_index = new_effect(&object->location, object->polygon, _effect_teleport_object_out, object->facing);
+	
+	if (effect_index==NONE)
+		return;
+	
+	effect_data *effect	= get_effect_data(effect_index);
+	Object *effect_object 	= get_object_data(effect->object_index);
+	
+	// make the effect look like the object
+	effect_object->shape		= object->shape;
+	effect_object->sequence		= object->sequence;
+	effect_object->transfer_mode	= _xfer_fold_out;
+	effect_object->transfer_period	= TELEPORTING_MIDPOINT;
+	effect_object->transfer_phase	= 0;
+	effect_object->flags		|= object->flags & (_object_is_enlarged|_object_is_tiny);
+	
+	// make the object invisible
+	SET_OBJECT_INVISIBILITY(object, true);
+
+	play_object_sound(effect->object_index, Sound_TeleportOut()); /* teleport in sound, at destination */
 }
 
 // if the given object isnÕt already teleporting in, do so
-void teleport_object_in(
-	short object_index)
+void teleport_object_in(short object_index)
 {
 	struct effect_data *effect;
 	short effect_index;
