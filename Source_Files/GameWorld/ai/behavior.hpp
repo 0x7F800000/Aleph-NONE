6709bf0e5374d911f4f25108d2efd0c435f7cb4f
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-06 16:58:48 -0500

    Update behavior.hpp

--- Source_Files/GameWorld/ai/behavior.hpp
+++ Source_Files/GameWorld/ai/behavior.hpp
@@ -1,7 +1,23 @@
 
+
 class monsterBehavior
 {
 public:
  monsterBehavior() {}
- virtual void behave(Monster *me) = 0;
+ inline int operator()(Monster *me)
+ {
+  return behave(me);
+ }
+ 
+private:
+ virtual int behave(Monster *me) = 0;
+ /*
+  the name of the behavior
+ */
+ char* behaviorName;  
+ 
+ /*
+  the behavior's class
+ */
+ char* behaviorClass;
 };
