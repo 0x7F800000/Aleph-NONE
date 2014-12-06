69137abc7630e6ace1f082da0bbaf87ac94ea2ad
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-06 18:24:58 -0500

    added behaviorPrecedence

--- Source_Files/GameWorld/ai/behavior.hpp
+++ Source_Files/GameWorld/ai/behavior.hpp
@@ -4,18 +4,29 @@ class monsterBehavior
 {
 public:
  monsterBehavior() {}
+ virtual int behave(class Monster *me, ...) = 0;
  
  inline const char* getName()
  {
   return behaviorName;
  }
+ 
  inline const char* getClass()
  {
   return behaviorClass;
  }
  
-private:
- virtual int behave(class Monster *me, ...) = 0;
+ inline bool isExclusive()
+ {
+  return !behaviorPrecedence;
+ }
+ 
+ inline uint32 getPrecedence()
+ {
+  return behaviorPrecedence;
+ }
+ 
+protected:
  /*
   the name of the behavior
  */
@@ -25,4 +36,28 @@ private:
   the behavior's class
  */
  char* behaviorClass;
+ /*
+  behaviorPrecendence determines which behaviors of a class are exhibited before the others
+  the lower the number, the more likely the behavior is to be exhibited before another behavior of its class
+  
+  ex: two behaviors of behaviorClass "Movement"
+  {
+   behaviorName: "Fly",
+   behaviorClass: "Movement",
+   behaviorPrecedence: 2
+  }
+  {
+   behaviorName: "Float",
+   behaviorClass: "Movement",
+   behaviorPrecedence: 1
+  }
+  the "Float" behavior will be called before the "Fly" behavior because Float has a higher precedence
+  
+  A behaviorPrecedence of 0 means that the behavior's class is exclusive in that there should only ever be
+  one behavior of that class attached to the Monster
+  
+  if a behavior with a precedence of 0 occupies the same class as another behavior that the Monster has,
+  an assertion should fail.
+ */
+ uint32 behaviorPrecedence;
 };
