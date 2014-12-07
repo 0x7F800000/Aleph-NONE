94a96dcfa9fd25451516e72a6335defdd03fc189
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-07 17:34:12 -0500

    Update d_emux86.h

--- Source_Files/GameWorld/damage/d_emux86.h
+++ Source_Files/GameWorld/damage/d_emux86.h
@@ -41,6 +41,61 @@ namespace x86Emu
 	{
 		return (val >> (bitsizeof(T) - 1)) & 1;
 	}
+	
+	
+	class x86Flags
+	{
+	public:
+		#define		__declflag(flag, name)	inline void set##name##(bool status) \
+			{	flag = (status) ? 1 : 0;	}\
+			inline bool ##name##()	{	return bool(flag);	}
+			
+		x86Flags() {}
+		
+		__declflag(carry, Carry)
+		__declflag(parity, Parity)
+		__declflag(adjust, Adjust)
+		__declflag(zero, Zero)
+		__declflag(sign, Sign)
+		__declflag(trap, Trap)
+		__declflag(direction, Direction)
+		__declflag(overflow, Overflow)
+		
+		inline size_t getFlags()
+		{
+			return __flags;
+		}
+		inline size_t setFlags(size_t set)
+		{
+			return __flags = set;
+		}
+		
+	private:
+	
+		#define	__declbit(name)	size_t name : 1
+		
+		union
+		{
+			struct
+			{
+				__declbit(carry),
+				__declbit(reserved0),
+				__declbit(parity),
+				__declbit(reserved1),
+				__declbit(adjust),
+				__declbit(reserved2),
+				__declbit(zero),
+				__declbit(sign),
+				__declbit(trap),
+				__declbit(interruptEnable),
+				__declbit(direction),
+				__declbit(overflow)
+			};	//the rest of the flags are unneeded
+			size_t __flags;
+		};
+	};
+	static x86Flags flags;
+	
 	class x86Register
 	{
 	public:
@@ -70,9 +125,9 @@ namespace x86Emu
 				static_cast<std::make_unsigned<decltype(v)>::type>(v) << highByteShift \
 				: v
 					
-		#define		__declopr(type)		template<typename T, bool low = true> type
+		#define		__declopr(type, ...)	template<typename T, bool low = true, __VA_ARGS__> type
 		
-		#define		UNSIGNED_OPR_PROLOG(fullreg, castreg, )\
+		#define		UNSIGNED_OPR_PROLOG(fullreg, castreg)\
 			LVARMASK();\
 			size_t fullreg = uval & mask; \
 			ADJUST_BYTE_FOR_OP(fullreg);\
@@ -83,7 +138,7 @@ namespace x86Emu
 			uval = maskedRegOprU(fullreg);\
 			return castreg
 			
-		#define		SIGNED_OPR_PROLOG(fullreg, castreg, )\
+		#define		SIGNED_OPR_PROLOG(fullreg, castreg)\
 			LVARMASK();\
 			decltype(this->sval) fullreg = uval & mask; \
 			ADJUST_BYTE_FOR_OP(fullreg);\
@@ -94,6 +149,9 @@ namespace x86Emu
 			sval = maskedRegOprS(fullreg);\
 			return castreg	
 			
+		#define		ARGUMENT_IS_REGISTER(argument)	\
+		( std::is_same< decltype(this), decltype(argument) >::value)
+		
 		__declopr(T) increment()
 		{
 			if( INTEGRAL() )
@@ -121,36 +179,98 @@ namespace x86Emu
 			uval -= SIZEOF_REFERENCED_TYPE();
 			return T(uval);
 		}
+		#define	__declshift(fname, castname, operation, signedness, capital_signedness)\
+		__declopr(T) fname(signedness factor)\
+		{\
+			##capital_signedness##_OPR_PROLOG(fullbits, castname);\
+			operation;\
+			##capital_signedness##_OPR_EPILOG(fullbits, castname);\
+		}
 		
 		//unsigned shift right
-		__declopr(T) ushr(unsigned factor)
+		__declshift(ushr, casted, casted >>= factor, unsigned, UNSIGNED)
+		//unsigned shift left
+		__declshift(ushl, casted, casted <<= factor, unsigned, UNSIGNED)
+		
+		//signed shift right
+		__declshift(sshr, casted, casted >>= factor, signed, SIGNED)
+		//signed shift left
+		__declshift(sshl, casted, casted <<= factor, signed, SIGNED)
+		
+		__declopr(T) Value()
 		{
 			UNSIGNED_OPR_PROLOG(fullbits, casted);
-			casted >>= factor;
+			UNSIGNED_OPR_EPILOG(fullbits, casted);
+		}
+		__declopr(void) compare(T against)
+		{
+			
+		}
+		
+		/*
+			sets the reg/one of its sub regs to a Value and extends the sign to the whole register
+			if extendSign is true
+		*/
+		__declopr(T, bool extendSign = false) Value(T newValue)
+		{
+			UNSIGNED_OPR_PROLOG(fullbits, casted);
+			if( sizeof(T) == sizeof(int16) && extendSign)
+			{
+				if(std::is_unsigned<T>::value)
+					uval = static_cast< decltype(uval) >(newValue);
+				else if(std::is_signed<T>::value)
+					sval = static_cast< decltype(sval) >(newValue);
+				else
+					assert(false);
+				return newValue;
+			}
 			UNSIGNED_OPR_EPILOG(fullbits, casted);
 		}
 		
-		//unsigned shift left
-		__declopr(T) ushl(unsigned factor)
+		__declopr(T) bor(T bits)
 		{
 			UNSIGNED_OPR_PROLOG(fullbits, casted);
-			casted <<= factor;
+			casted |= bits;
 			UNSIGNED_OPR_EPILOG(fullbits, casted);
 		}
-		//signed shift right
-		__declopr(T) sshr(signed factor)
+		
+		__declopr(T) band(T bits)
+		{
+			UNSIGNED_OPR_PROLOG(fullbits, casted);
+			casted &= bits;
+			UNSIGNED_OPR_EPILOG(fullbits, casted);
+		}
+		
+		__declopr(T) bxor(T bits)
+		{
+			UNSIGNED_OPR_PROLOG(fullbits, casted);
+			casted ^= bits;
+			UNSIGNED_OPR_EPILOG(fullbits, casted);
+		}
+		
+		__declopr(T) bnot()
+		{
+			UNSIGNED_OPR_PROLOG(fullbits, casted);
+			casted = ~casted;
+			UNSIGNED_OPR_EPILOG(fullbits, casted);
+		}
+		__declopr(T) neg()
 		{
 			SIGNED_OPR_PROLOG(fullbits, casted);
-			casted >>= factor;
+			casted = -casted;
 			SIGNED_OPR_EPILOG(fullbits, casted);
 		}
-		//signed shift left
-		__declopr(T) sshl(signed factor)
+		
+		__declopr(bool) test(T testVal)
 		{
-			SIGNED_OPR_PROLOG(fullbits, casted);
-			casted <<= factor;
-			SIGNED_OPR_EPILOG(fullbits, casted);
+			UNSIGNED_OPR_PROLOG(fullbits, casted);
+			
+			if( !ARGUMENT_IS_REGISTER(testVal) )
+				return casted & testVal;
+			T otherVal = testVal.Value<T, low>();
+			return casted & otherVal;
 		}
+		
 		__declopr(unsigned) maskForType()
 		{
 			static_assert( std::is_integral<T>::value, "maskForType requires an integral typename" );
@@ -176,5 +296,5 @@ namespace x86Emu
 			size_t uval;
 			ptrdiff_t sval;
 		};
-	}
+	};
 };
