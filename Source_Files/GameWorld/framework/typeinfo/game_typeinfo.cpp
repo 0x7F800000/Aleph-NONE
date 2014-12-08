54ed242a117e02803b59c89df2bfe09095cf7ad5
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-08 17:50:19 -0500

    Update game_typeinfo.cpp

--- Source_Files/GameWorld/framework/typeinfo/game_typeinfo.cpp
+++ Source_Files/GameWorld/framework/typeinfo/game_typeinfo.cpp
@@ -2,3 +2,39 @@
 	GAME_TYPEINFO.CPP
 	
 */
+#include "cseries.h"
+
+#include "map.h"
+#include "effects.h"
+#include "monsters.h"
+#include "projectiles.h"
+#include "player.h"
+#include "scenery.h"
+#include "platforms.h"
+#include "lightsource.h"
+#include "media.h"
+#include "items.h"
+#include "weapons.h"
+#include "world.h"
+
+#include "./damage/doors.hpp"
+
+#include <type_info>
+#include <type_traits>
+
+#include <string>
+#include <limits.h>
+
+#include "game_typeinfo.hpp"
+
+#define		__declmemberInfo(classname, membername)	\
+		\
+		alephType::alephMemberInfo<\
+		alephType::offset_of<classname, decltype(classname::membername), &classname::membername>()\
+		, decltype(classname::membername)>
+
+void alephType::initTypeInfo()
+{
+	__declmemberInfo(Monster, type) test;
+}
+
