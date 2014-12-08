0826a267210c6e0f843ae62b5f697e5cfc8209e6
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-08 17:22:00 -0500

    added alephType::offset_of

--- Source_Files/GameWorld/framework/typeinfo/game_typeinfo.hpp
+++ Source_Files/GameWorld/framework/typeinfo/game_typeinfo.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+namespace alephType
+{
+	/*
+		like the offsetof macro, but evaluated at compile-time
+	*/
+	template< typename classType, typename memberType, memberType classType::*member > 
+	static constexpr size_t offset_of()
+	{
+		return reinterpret_cast<size_t>( &reinterpret_cast<T*>(nullptr)->*M );
+	}
+};
