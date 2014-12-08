1d232b9253b251c316be78020bd6daef9bc9d0b8
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-08 16:05:37 -0500

    Create gameworld_exception.hpp

---
+++ Source_Files/GameWorld/framework/error/gameworld_exception.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/*
+	Currently a stub header. Intended to eventually replace assertions that may fail as a result
+	of lua scripts/XML scripts doing nasty stuff.
+	
+	GameWorldException will bring the game itself to a halt and provide an error message to the player,
+	while not terminating the application. 
+	
+	Will probably also have code for more informative error reporting.
+	
+*/
