539ec842ca02ff3345ac19703ab292390ad4530c
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-07 22:31:27 -0500

    Create swinging_door_definitions.hpp

---
+++ Source_Files/GameWorld/damage/swinging_door_definitions.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+struct swinging_door_definition
+{
+	uint8 field_0[30];
+	int16 field_1E;
+	int16 textured_object_0_definition_index;
+	int16 textured_object_1_definition_index;
+	int16 oops;
+	int16 field_26;
+	int16 field_28;
+	int16 sound0;
+	int16 sound2;
+	int16 sound1;
+};
+
+#ifndef DONT_REPEAT_DEFINITIONS
+
+static const swinging_door_definition original_swinging_door_definitions[] =
+{
+};
+
+#endif
+
