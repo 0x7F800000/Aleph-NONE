463a72e7093542208f49026f8d69e76e8fd630c7
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-07 19:21:59 -0500

    Update emux86.cpp

--- Source_Files/GameWorld/damage/emux86.cpp
+++ Source_Files/GameWorld/damage/emux86.cpp
@@ -1,2 +1,30 @@
 /*
+	EMUX86.CPP
+	
+	Code for emulating complex code sequences in Damage Inc that won't decompile well
+	and probably never will
+	
+	with time this will eventually be phased out
 */
+
+#include "cseries.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "emux86.hpp"
+
+using namespace x86Emu;
+
+x86Flags x86Emu::flags;
+
+x86Register X86Emu::EAX;
+x86Register X86Emu::EBX;
+x86Register X86Emu::ECX;
+x86Register X86Emu::EDX;
+x86Register X86Emu::EDI;
+x86Register X86Emu::ESI;
+x86Register X86Emu::EBP;
+
+
