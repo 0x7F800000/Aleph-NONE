bd6ac9c484d7677b80acf1edce3d469b37a31908
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-08 18:09:50 -0500

    Update game_typeinfo.hpp

--- Source_Files/GameWorld/framework/typeinfo/game_typeinfo.hpp
+++ Source_Files/GameWorld/framework/typeinfo/game_typeinfo.hpp
@@ -2,25 +2,5 @@
 
 namespace alephType
 {
-	/*
-		like the offsetof macro, but evaluated at compile-time
-	*/
-	template< typename classType, typename memberType, memberType classType::*member > 
-	static constexpr size_t offset_of()
-	{
-		classType* classPtr = nullptr;
-		return reinterpret_cast<size_t>( &(*classPtr).(*member) );
-	}
-	
 	void initTypeInfo();
-	
-	template< size_t memberOffset, typename memberTypename> struct alephMemberInfo
-	{
-		const std::type_info& getInfo()
-		{
-			return typeid(memberTypename);
-		}
-		const size_t offset = memberOffset;
-	};
-	
 };
