97273fa06171f06d048dd098271199a80835822b
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-07 20:55:54 -0500

    Update emux86.cpp

--- Source_Files/GameWorld/damage/emux86.cpp
+++ Source_Files/GameWorld/damage/emux86.cpp
@@ -16,17 +16,6 @@
 #include "emux86.hpp"
 
 
-
-x86Emu::x86Flags x86Emu::flags;
-
-x86Emu::x86Register x86Emu::EAX;
-x86Emu::x86Register x86Emu::EBX;
-x86Emu::x86Register x86Emu::ECX;
-x86Emu::x86Register x86Emu::EDX;
-x86Emu::x86Register x86Emu::EDI;
-x86Emu::x86Register x86Emu::ESI;
-x86Emu::x86Register x86Emu::EBP;
-
 void fooo()
 {
 	x86Emu::EAX.compare<int16, false>(0xFFFF);
