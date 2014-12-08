8bef67d4a4ec3de5b6dfa3e1a86c4d77388baa67
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-08 17:54:27 -0500

    Update game_typeinfo.hpp

--- Source_Files/GameWorld/framework/typeinfo/game_typeinfo.hpp
+++ Source_Files/GameWorld/framework/typeinfo/game_typeinfo.hpp
@@ -8,7 +8,7 @@ namespace alephType
 	template< typename classType, typename memberType, memberType classType::*member > 
 	static constexpr size_t offset_of()
 	{
-		return reinterpret_cast<size_t>( &reinterpret_cast<classType*>(nullptr)->*member );
+		return reinterpret_cast<size_t>( &static_cast<classType*>(nullptr)->*member );
 	}
 	
 	void initTypeInfo();
