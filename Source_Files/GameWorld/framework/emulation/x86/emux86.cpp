79aa65a39099f152793f443f79a54f24912985b8
0x7F800000 <vw6j0+5qe874nq1v6xg@sharklasers.com>
2014-12-08 16:09:02 -0500

    moved x86 emulation into framework directory

---
+++ Source_Files/GameWorld/framework/emulation/x86/emux86.cpp
@@ -0,0 +1,16 @@
+/*
+	EMUX86.CPP
+	
+	Code for emulating complex code sequences in Damage Inc that won't decompile well
+	and probably never will
+	
+	with time this will eventually be phased out
+*/
+
+#include "cseries.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <type_traits>
+#include "emux86.hpp"
